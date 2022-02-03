7e016e048f1d10e0edc8741cdfdf5502c8dd724a
“Victoria <victoriahall.dev@gmail.com>
2022-02-03 18:47:00 +0000

    displayed product and category count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"], password: ENV["HTTP_BASIC_PASSWORD"]
 
   def show
-
+   @product_count = Product.count
+@category = Product.group(:category_id).count
   end
 end
