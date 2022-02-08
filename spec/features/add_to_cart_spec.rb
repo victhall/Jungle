16d279c8cc5b716ef0cd90cc08be7011a3af85d2
“Victoria <victoriahall.dev@gmail.com>
2022-02-08 22:11:39 +0000

    implemented feature spec for add to cart

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor can click the 'Add to Cart' button for a product on the home page and their cart increases by one", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see their cart increases by one" do
+    # ACT
+    visit root_path
+
+    # DEBUG
+    save_screenshot
+
+    # VERIFY
+    expect(page).to have_text('My Cart (0)')
+
+    click_on('Add', match: :first)
+
+    expect(page).to have_text('My Cart (1)')
+
+    # DEBUG
+    save_screenshot
+  end
+end
